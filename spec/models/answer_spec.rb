39720206e6a1426692f8f5398f635549c74dad7b
Stephanie <stephaniepui@outlook.com>
2015-08-24 20:16:37 -0700

    Assignment 28 completed

---
+++ spec/models/answer_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Answer, type: :model do
+  context "attributes" do
+     let(:question) { Question.create!(title: "New Question Title", body: "New Question Body") }
+     let(:answer) { Answer.create!(body: 'Answer Body', question: question) }
+
+     it "should respond to body" do
+       expect(answer).to respond_to(:body)
+     end
+   end
+end
