748547ac83c67ef3736493d79f30485b9227b613
Stephanie <stephaniepui@outlook.com>
2015-08-27 20:58:05 -0700

    Assignment 31 completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,13 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+
+    @posts.each_with_index do |post, index|
+      if index % 5 == 4
+        post.update(title: 'CENSORED')
+      end
   end
+end
 
   def show
   end
