19d2140ce47de4cf01918413f531c38f99d20c74
Stephanie <stephaniepui@outlook.com>
2015-09-05 13:20:22 -0700

    Checkpoint 39 comleted, Posts and Users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,16 @@
 include RandomData
 
+# Create Users
+5.times do
+  user = User.create!(
+# #3
+  name:     RandomData.random_name,
+  email:    RandomData.random_email,
+  password: RandomData.random_sentence
+  )
+end
+users = User.all
+
 # Create Topics
 15.times do
   Topic.create!(
@@ -14,7 +25,7 @@ topics = Topic.all
  50.times do
  # #1
    Post.create!(
- # #2
+     user: users.sample,
      topic: topics.sample,
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
@@ -32,7 +43,14 @@ topics = Topic.all
    )
  end
 
+ user = User.first
+ user.update_attributes!(
+  email: 'stephaniepui@outlook.com',
+  password: 'helloworld'
+ )
+
  puts "Seed finished"
+ puts "#{User.count} users created"
  puts "#{Topic.count} topics created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
