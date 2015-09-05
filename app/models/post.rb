ba149d5b5cdf21035210e4091c76063e4039581c
Stephanie <stephaniepui@outlook.com>
2015-09-05 14:58:39 -0700

    Assignment 39 completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,10 +4,15 @@ class Post < ActiveRecord::Base
     has_many :comments, dependent: :destroy
 
     default_scope { order('created_at DESC') }
+    scope :ordered_by_title, -> { reorder(:title)}
+    scope :ordered_by_reverse_created_at, -> { reorder('created_at ASC') }
 
     validates :title, length: { minimum: 5 }, presence: true
     validates :body, length: { minimum: 20 }, presence: true
     validates :topic, presence: true
     validates :user, presence: true
+
+
+
 
 end
