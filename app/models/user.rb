241169b3efecf1cd22cc44851d57703d38dcfbd0
Stephanie <stephaniepui@outlook.com>
2015-09-05 17:46:23 -0700

    Checkpoint 40 competed, Authorization

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_many :posts
   before_save { self.email = email.downcase }
+  before_save { self.role ||= :member }
 
 # #3
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -8,7 +9,7 @@ class User < ActiveRecord::Base
 # #4
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
 # #5
-  validates :password, presence: true, length: { minimum: 6 }
+
 # #6
   validates :email,
             presence: true,
@@ -16,7 +17,11 @@ class User < ActiveRecord::Base
             length: { minimum: 3, maximum: 100 },
             format: { with: EMAIL_REGEX }
 
+    validates :password, presence: true, length: { minimum: 6 }, on: :create
+
 # #7
   has_secure_password
+
+  enum role: [:member, :admin]
 
 end
