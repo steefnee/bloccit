7ec7f508e27da56486b432d5da1c468673c33dc0
Stephanie <stephaniepui@outlook.com>
2015-09-06 22:37:15 -0700

    Checkpoint 42 completed, labels

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,7 +18,7 @@ class PostsController < ApplicationController
 
 # #10
     if @post.save
-# #11
+      @post.labels = Label.update_labels(params[:post][:labels])
       flash[:notice] = "Post was saved."
       redirect_to [@topic, @post]
     else
@@ -39,6 +39,7 @@ class PostsController < ApplicationController
     @post.assign_attributes(post_params)
 
     if @post.save
+      @post.labels = Label.update_labels(params[:post][:labels])
       flash[:notice] = "Post was updated."
       redirect_to [@post.topic, @post]
     else
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -5,7 +5,10 @@ RSpec.describe Topic, type: :model do
   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
 
   it { should have_many(:posts) }
-# #1
+  it { should have_many(:labelings) }
+# #11
+  it { should have_many(:labels).through(:labelings) }
+
   context "attributes" do
     it "should respond to name" do
       expect(topic).to respond_to(:name)
