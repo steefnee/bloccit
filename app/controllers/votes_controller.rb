be7f5941576c53efd53cce27e3e4f203057b4eac
Stephanie <stephaniepui@outlook.com>
2015-09-19 16:35:16 -0700

    Progress on assignment 47

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,13 +3,11 @@ class VotesController < ApplicationController
 
   def up_vote
     update_vote(1)
-    redirect_to :back
   end
 
 
   def down_vote
     update_vote(-1)
-    redirect_to :back
   end
 
 private
@@ -21,6 +19,10 @@ private
       @vote.update_attribute(:value, new_value)
     else
       @vote = current_user.votes.create(value: new_value, post: @post)
+    end
+    respond_to do |format|
+      format.html
+      format.js
     end
   end
 
